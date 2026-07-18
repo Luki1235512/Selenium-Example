e7096ef470a645b03b871402cca414dfcab6bd7b
Luki1235512 <luki1235512@gmail.com>
2026-07-18 15:49:49 +0200

    Add POM

--- src/test/java/com/automation/tests/LoginTest.java
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -1,19 +1,30 @@
 package com.automation.tests;
 
-import org.openqa.selenium.By;
+import com.automation.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
+  private WebDriver driver;
+
+  @BeforeMethod
+  public void setUp() {
+    driver = new ChromeDriver();
+    driver.get("https://www.saucedemo.com");
+  }
+
   @Test
   public void validLoginTest() {
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://www.saucedemo.com");
-    driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    driver.findElement(By.id("password")).sendKeys("secret_sauce");
-    driver.findElement(By.id("login-button")).click();
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.login("standard_user", "secret_sauce");
+  }
+
+  @AfterMethod
+  public void tearDown() {
     driver.quit();
   }
 }
