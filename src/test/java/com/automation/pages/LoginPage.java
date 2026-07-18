4fd5cd0e27af7d52f19f868d17e131b4477b72d5
Luki1235512 <luki1235512@gmail.com>
2026-07-18 16:31:46 +0200

    Add wait utils

--- src/test/java/com/automation/pages/LoginPage.java
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -1,30 +1,31 @@
 package com.automation.pages;
 
+import com.automation.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
-  private WebDriver driver;
+  private WaitUtils wait;
 
   private By usernameField = By.id("user-name");
   private By passwordField = By.id("password");
   private By loginButton = By.id("login-button");
 
   public LoginPage(WebDriver driver) {
-    this.driver = driver;
+    this.wait = new WaitUtils(driver);
   }
 
   public void enterUsername(String username) {
-    driver.findElement(usernameField).sendKeys(username);
+    wait.waitForVisible(usernameField).sendKeys(username);
   }
 
   public void enterPassword(String password) {
-    driver.findElement(passwordField).sendKeys(password);
+    wait.waitForVisible(passwordField).sendKeys(password);
   }
 
   public void clickLogin() {
-    driver.findElement(loginButton).click();
+    wait.waitForClickable(loginButton).click();
   }
 
   public void login(String username, String password) {
